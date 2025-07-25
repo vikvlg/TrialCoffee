257ea2431817bc06173fc3141033e7a061ad040e
vikvlg <linkvlg@yandex.ru>
2025-07-25 14:54:06 +0300

    Запрос обернут в try/catch, т.к. на телефоне не всегда успевала подгрузиться картинка. TODO: Нужно сделать подобное для всех запросов.

--- app/src/main/java/ru/vik/trials/coffee/data/GetImageRepositoryImpl.kt
+++ app/src/main/java/ru/vik/trials/coffee/data/GetImageRepositoryImpl.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.flow
 import ru.vik.trials.coffee.domain.GetImageRepository
 import ru.vik.trials.coffee.domain.entities.Image
 import ru.vik.trials.coffee.domain.entities.Resp
+import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 /** Реализация репозитория загрузки изображений. */
@@ -13,16 +14,23 @@ class GetImageRepositoryImpl @Inject constructor(
 ) : GetImageRepository {
 
     override fun getImage(url: String): Flow<Resp<Image>> = flow {
-        val res = service.downloadImage(url)
-        if (res.isSuccessful) {
-            res.body()?.let {
-                emit(Resp(Image(url,it.bytes())))
-            } ?: run {
-                // "Неизвестная ошибка"
-                emit(Resp(400))
+        try {
+            val res = service.downloadImage(url)
+            if (res.isSuccessful) {
+                res.body()?.let {
+                    emit(Resp(Image(url, it.bytes())))
+                } ?: run {
+                    throw Exception()
+                }
+            } else {
+                emit(Resp(res.code()))
             }
-        } else {
-            emit(Resp(res.code()))
+        }
+        catch (_: SocketTimeoutException) {
+            emit(Resp(HttpErrors.TIMEOUT))
+        }
+        catch (_: Exception) {
+            emit(Resp(HttpErrors.UNKNOWN))
         }
     }
 }
