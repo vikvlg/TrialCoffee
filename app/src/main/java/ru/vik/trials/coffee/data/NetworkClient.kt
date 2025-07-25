f3015a23a7502372cf01d562f070d1a98f4e672b
vikvlg <linkvlg@yandex.ru>
2025-07-25 14:54:53 +0300

    Таймауты установлены в 30 секунд (по умолчанию 10 - для телефона маловато).

--- app/src/main/java/ru/vik/trials/coffee/data/NetworkClient.kt
+++ app/src/main/java/ru/vik/trials/coffee/data/NetworkClient.kt
@@ -1,10 +1,13 @@
 package ru.vik.trials.coffee.data
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.vik.trials.coffee.BuildConfig
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
+
 
 /**
  * Клиент для работы с сетью.
@@ -13,6 +16,10 @@ import javax.inject.Singleton
  * */
 @Singleton
 class NetworkClient @Inject constructor() {
+    companion object {
+        /** Таймаут на операции с сетью, сек. */
+        const val TIMEOUT = 30L
+    }
 
     val provideRetrofit = buildRetrofit()
 
@@ -20,6 +27,13 @@ class NetworkClient @Inject constructor() {
 
     internal fun buildRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl(BuildConfig.API_URL)
+        .client(getHttpClient())
         .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private fun getHttpClient(): OkHttpClient = OkHttpClient().newBuilder()
+        .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
         .build()
 }
